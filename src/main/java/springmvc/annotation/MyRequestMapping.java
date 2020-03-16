3e878946015053f78b751392506d824f24101946
tideseng <justdoit_onebyone@163.com>
2020-03-16 23:45:11 +0800

    自定义spring注解

---
+++ src/main/java/springmvc/annotation/MyRequestMapping.java
@@ -0,0 +1,12 @@
+package springmvc.annotation;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyRequestMapping {
+
+    String value() default "";
+
+}
